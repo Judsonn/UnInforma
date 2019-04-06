497df92535631fcf0961e2f4ea661e8b92c572a2
Sabrina <sabrinacarlew@gmail.com>
2019-04-05 21:19:13 -0300

    Projeto Netbeans e Classes Base

---
+++ UnInforma/src/controller/ClienteHttp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Sabrina Winckler
+ */
+
+//Classe que faz a conexão com o servidor e o GET nos dados
+public class ClienteHttp {
+    
+}
