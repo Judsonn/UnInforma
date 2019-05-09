a99334320dff53063114844e6574f3bf8cbbf1bb
judson <judson.henrique01@gmail.com>
2019-05-09 16:53:02 -0300

    CSVs

--- UnInforma/src/model/Curso.java
+++ UnInforma/src/model/Curso.java
@@ -10,7 +10,55 @@ package model;
  * @author Sabrina Winckler
  */
 public class Curso {
-    public void test(){  
-    System.out.println("chegou");
+    private String nome;
+    private String turno;
+    private float qntSemestre;
+    private double chTotal;
+
+    public Curso(String nome, String turno, float qntSemestre, double chTotal) {
+        this.nome = nome;
+        this.turno = turno;
+        this.qntSemestre = qntSemestre;
+        this.chTotal = chTotal;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public float getQntSemestre() {
+        return qntSemestre;
+    }
+
+    public void setQntSemestre(float qntSemestre) {
+        this.qntSemestre = qntSemestre;
+    }
+
+    public double getChTotal() {
+        return chTotal;
+    }
+
+    public void setChTotal(double chTotal) {
+        this.chTotal = chTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso:" + "nome=" + nome + "/n turno=" + turno + ",/n qntSemestre=" + qntSemestre + ",/n chTotal=" + chTotal + '}';
+    }
+    
+ 
+    
 }
